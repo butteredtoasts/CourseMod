ff58dea69556759620e308a771b16923d87c6981
Tazz Akber <90526357+butteredtoasts@users.noreply.github.com>
2024-10-06 13:06:49 -0400

    Compostables added: - Tomato - Tomato Seeds

--- src/main/java/com/butteredtoasts/coursemod/datagen/ModDataMapProvider.java
+++ src/main/java/com/butteredtoasts/coursemod/datagen/ModDataMapProvider.java
@@ -1,8 +1,11 @@
 package com.butteredtoasts.coursemod.datagen;
 
+import com.butteredtoasts.coursemod.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +16,9 @@ public class ModDataMapProvider extends DataMapProvider {
 
     @Override
     protected void gather() {
+        this.builder(NeoForgeDataMaps.COMPOSTABLES)
+                .add(ModItems.TOMATO.getId(), new Compostable(.45f), false)
+                .add(ModItems.TOMATO_SEEDS.getId(), new Compostable(.25f), false);
         /*this.builder(NeoForgeDataMaps.FURNACE_FUELS)
                 .add(ModItems.STARLIGHT_ASHES.getId(), new FurnaceFuel(1200), false)
                 .add(ModItems.FROSTFIRE_ICE.getId(), new FurnaceFuel(2400), false);*/
